64927c191480afab19dc0644fd6e685075b719b6
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-10-15 16:22:13 +0300

    added: assets

---
+++ assets/index.js
@@ -0,0 +1,17 @@
+export const imageSrc = {
+  homeIconImage:
+    "https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450",
+  categories: {
+    image1: "https://links.papareact.com/gn7",
+    image2: "https://links.papareact.com/gn7",
+    image3: "https://links.papareact.com/gn7",
+    image4: "https://links.papareact.com/gn7",
+    image5: "https://links.papareact.com/gn7",
+    image6: "https://links.papareact.com/gn7",
+  },
+  restaurents: {
+    image1: "https://links.papareact.com/gn7",
+    image2: "https://links.papareact.com/gn7",
+    image2: "https://links.papareact.com/gn7",
+  },
+};
